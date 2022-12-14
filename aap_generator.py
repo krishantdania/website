7fd6ebe6ea87d303d9c66e0d961ff3336d4a19b6
krishantdania <106925765+krishantdania@users.noreply.github.com>
2022-12-14 17:36:27 -0500

    ASAP

---
+++ aap_generator.py
@@ -0,0 +1,8 @@
+# Start by prompting the user to enter a word
+word = input("Enter a word: ")
+
+# Add "A$AP" in front of the user-entered word
+a_ap_word = "A$AP " + word
+
+# Print the resulting A$AP-ified word
+print(a_ap_word)
